8c895f76c18872c164d6208cbc803a0f37e200ac
jhelms85 <156390402+jhelms85@users.noreply.github.com>
2024-03-25 16:56:41 -0400

    create pages for project

---
+++ itis3135/project/scripts/script.js
@@ -0,0 +1,7 @@
+/* Function to remove styling when CSS image is clicked */
+function removeCSS() {
+    var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+    stylesheets.forEach(function(stylesheet) {
+        stylesheet.disabled = true;
+    });
+}
