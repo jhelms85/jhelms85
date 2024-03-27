1bede92a589e44ad7f0bc5dd7dfaa6079d6e3698
jhelms85 <156390402+jhelms85@users.noreply.github.com>
2024-03-26 20:25:33 -0400

    Update script.js

--- itis3135/project/scripts/script.js
+++ itis3135/project/scripts/script.js
@@ -1,5 +1,10 @@
 /* Function to remove styling when CSS image is clicked */
 function removeCSS() {
+    var confirmed = window.confirm("Are you sure you want to remove the styling? This is useful if you are having compatability issues. Refresh the page to restore the styling.");
+    if (!confirmed) {
+        return;
+    }
+
     var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
     stylesheets.forEach(function(stylesheet) {
         stylesheet.disabled = true;
